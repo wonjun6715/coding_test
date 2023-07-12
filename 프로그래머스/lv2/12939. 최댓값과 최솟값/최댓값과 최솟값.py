b211657edd4fde42f479b4ad512c6fe696509f32
이원준 <wj6715@naver.com>
2023-07-12 10:23:22 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.05 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
+++ 프로그래머스/lv2/12939. 최댓값과 최솟값/최댓값과 최솟값.py
@@ -1,6 +1,4 @@
 def solution(s):
     answer = ''
-    result = s.split() # 공백을 기준으로 리스트에 넣음
-    for i in range(len(result)):
-        result[i] = int(result[i])
+    result = list(map(int, s.split()))
     return f'{min(result)} {max(result)}'
