6c182368885114bb306850dc3ea6f542b27c413d
이원준 <wj6715@naver.com>
2023-08-14 23:44:33 +0900

    [unrated] Title: 세 개의 구분자, Time: 8.19 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.py
+++ 프로그래머스/unrated/181862. 세 개의 구분자/세 개의 구분자.py
@@ -1,16 +1,3 @@
 def solution(myStr):
-    answer = []
-    key = ''
-    myStr += 'a'
-    cnt = 0
-    for i in range(len(myStr)):
-        if myStr[i] == 'a' or myStr[i] == 'b' or myStr[i] == 'c':
-            if key != '':
-                answer.append(key)
-                key = ''
-                cnt += 1
-        else:
-            key += myStr[i]
-    if cnt == 0:
-        return ["EMPTY"]
-    return answer
+    answer = [x for x in myStr.replace('a', ' ').replace('b', ' ').replace('c', ' ').split() if x]
+    return answer if answer else ['EMPTY']
