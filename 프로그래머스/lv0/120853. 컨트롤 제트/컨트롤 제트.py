bf568d0d56543d7054b3d7b5748ebab5268de29c
이원준 <wj6715@naver.com>
2023-08-17 18:55:43 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.py
@@ -1,18 +1,9 @@
-def is_digit(str):
-    try:
-        tmp = float(str)
-        return True
-    except ValueError:
-        return False
 def solution(s):
-    answer = s.split()
-    result = 0
-    tmp = 0
-    for i in range(len(answer)):        
-        if is_digit(answer[i]):
-            answer[i] = int(answer[i])
-            result += answer[i]
-            tmp = answer[i]
+    stack = []
+    for a in s.split():
+        if a != 'Z':
+            stack.append(int(a))
         else:
-            result -= tmp
-    return result
+            stack.pop()
+            
+    return sum(stack)
