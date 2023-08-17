39bbcb3e7b535472e056b6fd139fa3bbbff248de
이원준 <wj6715@naver.com>
2023-08-17 20:04:06 +0900

    [unrated] Title: 문자열 묶기, Time: 8.76 ms, Memory: 17.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
+++ 프로그래머스/unrated/181855. 문자열 묶기/문자열 묶기.py
@@ -1,10 +1,5 @@
 def solution(strArr):
-    answer = []
-    max = 0
-    for i in range(len(strArr)):
-        answer.append(len(strArr[i]))
-    result = list(set(answer))
-    for i in range(len(result)):
-        if max < answer.count(result[i]):
-            max = answer.count(result[i])
-    return max
+    answer = [0]*31
+    for x in strArr:
+        answer[len(x)] += 1
+    return max(answer)
