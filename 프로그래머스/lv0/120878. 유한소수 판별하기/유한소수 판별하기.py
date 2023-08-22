f38eab72f21444c74caf0ebd972a2f3bc3b233bd
이원준 <wj6715@naver.com>
2023-08-22 22:56:57 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.py
@@ -4,27 +4,10 @@ def gcd(x, y):
     else:
         return gcd(y, x % y)
 def solution(a, b):
-    answer = []
-    d = 2
-    common = gcd(a, b)
-    a_1 = a // common
-    b_1 = b // common
-    
-    while d <= b_1:
-        if b_1 % d == 0:
-            b_1 = b_1 // d
-            answer.append(d)
-        else:
-            d += 1   
-    result = list(set(answer))
-    if len(result) == 2 and 2 in result and 5 in result:
-        return 1
-    elif len(result) == 1 and result[0] == 2:
-        return 1
-    elif len(result) == 1 and result[0] == 5:
-        return 1
-    elif a  % b == 0:
-        return 1
-    else:
-        return 2
+    b = b // gcd(a,b)
+    while b % 2 == 0:
+        b = b // 2
+    while b % 5 == 0:
+        b = b // 5
+    return 1 if b == 1 else 2
     
