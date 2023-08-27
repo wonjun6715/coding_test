73683aa3c8ef99c24539b838e6aa0b5e962c3046
이원준 <wj6715@naver.com>
2023-08-27 23:30:11 +0900

    [level 1] Title: 숫자 짝꿍, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/131128. 숫자 짝꿍/숫자 짝꿍.py
@@ -0,0 +1,17 @@
+from collections import Counter
+def solution(X, Y):
+    answer = ''
+    X = Counter(X)
+    Y = Counter(Y)
+    
+    for i in range(9, -1, -1):
+        mn = min(X[str(i)], Y[str(i)])
+        answer += str(i) * mn
+    
+    if answer == '':
+        return '-1'
+    if answer[0] == '0':
+        return '0'
+        
+        
+    return answer
