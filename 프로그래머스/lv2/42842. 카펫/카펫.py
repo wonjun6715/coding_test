50a56c1d34315d001b9a586e42f7d6d215b200d5
이원준 <wj6715@naver.com>
2023-09-07 17:22:33 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -0,0 +1,8 @@
+def solution(brown, yellow):
+    answer = []
+    size = brown + yellow
+    for i in range(size+1, -1, -1):
+        if size % i == 0:
+            div = size // i
+            if (i - 2) * (div -2) == yellow:
+                return[i, div]
