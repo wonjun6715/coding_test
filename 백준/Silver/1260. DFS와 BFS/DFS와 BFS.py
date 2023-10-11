2d741986fb051eafb61ea4cfa027d6403d663cbc
이원준 <wj6715@naver.com>
2023-10-12 03:04:07 +0900

    [Silver II] Title: DFS와 BFS, Time: 472 ms, Memory: 34096 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -2,32 +2,32 @@ from collections import deque
 def dfs(graph, v, visited):
     visited[v] = True
     print(v, end = ' ')
+    
     for i in graph[v]:
-        if not visited[i]:
+        if visited[i] == False:
             dfs(graph, i, visited)
-
+            
 def bfs(graph, start, visited):
     queue = deque([start])
     visited[start] = True
-    
+
     while queue:
         v = queue.popleft()
         print(v, end = ' ')
         for i in graph[v]:
-            if not visited[i]:
+            if visited[i] == False:
                 queue.append(i)
                 visited[i] = True
-                
-n, m, v = map(int, input().split()) # n은 정점의 개수, m은 간선의 개수, v는 시작 지점
- 
+n,m,v = map(int, input().split())
+
 graph = [[] for _ in range(n + 1)]
+
 for i in range(m):
-    a, b = map(int, input().split())
+    a, b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
-    
-for i in range(n+1):
-    graph[i].sort() # 가장 작은 번호부터 방문해야 하므로 정렬
+for i in range(1, n + 1):
+    graph[i].sort()
     
 visited_dfs = [False] * (n + 1)
 visited_bfs = [False] * (n + 1)
