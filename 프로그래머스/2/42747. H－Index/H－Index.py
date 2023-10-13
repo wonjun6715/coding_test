42f2b403dfa86c8c5f1558d61b3678bd058f5b5e
이원준 <wj6715@naver.com>
2023-10-13 13:48:59 +0900

    [level 2] Title: H-Index, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,6 +1,6 @@
-def solution(citations):
-    citations.sort(reverse = True)  
+def solution(citations): # citations = 피인용수
+    citations.sort(reverse=True)
     for i in range(len(citations)):
-        if citations[i] < (i + 1):
+        if citations[i] < (i + 1): # 피인용수가 논문수보다 작아지기 시작하는 숫자가 h
             return i
     return len(citations)
